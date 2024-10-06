d470da160a309020b7071460f35ea202d0f68a6d
MainHHH <bmn0330@gmail.com>
2024-10-06 20:03:07 +0900

    svelte 설치 + 라우터 만들기

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.question import question_router
 
 app = FastAPI()
 
-@app.get("/Hello")
-def hello():
-    return {"message": "안녕하세요"}
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(question_router.router)
