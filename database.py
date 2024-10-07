3928895d625ca632b3020f592ef8676f7ab6fda5
MainHHH <bmn0330@gmail.com>
2024-10-07 21:07:54 +0900

    어노테이션과 with 문으로 자동화

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import contextlib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,3 +11,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# 의존성 주입
+@contextlib.contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
